457431d4453358acf9987244556d815a8ec21626
mhmmdediefanto <ediefanto10@gmail.com>
2024-05-18 17:08:23 +0700

    addPermission

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -28,6 +28,152 @@ class PermissionsSeeder extends Seeder
                 'name' => 'view laporan',
                 'guard_name' => 'web',
             ],
+
+            [
+                //admin
+                'name' => 'view dashboard',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create barang atau jasa',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit barang atau jasa',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete barang atau jasa',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create kategori',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit kategori',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete kategori',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create diskon',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit diskon',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete diskon',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create pajak',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit pajak',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete pajak',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create biaya',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit biaya',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete biaya',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view pemasukan',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view pengeluaran',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view kategori pemasukan',
+                'guard_name' => 'web',
+            ], [
+                'name' =>  'create kategori pemasukan'
+            ], [
+                'name' => 'edit kategori pemasukan'
+            ], [
+                'name' => 'delete kategori pemasukan',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view kategori pengeluaran',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create kategori pengeluaran',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit kategori pengeluaran',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete kategori pengeluaran',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view laporan',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view laporan penjualan',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view laporan penjualan per barang',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view laporan pemasukan',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view laporan pengeluaran',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view menu',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create role',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit role',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete role',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view pengguna',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create pengguna',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit pengguna',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete pengguna',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view permission',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'create permission',
+                'guard_name' => 'web',
+
+            ], [
+                'name' => 'edit permission',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'delete permission',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'view toko',
+                'guard_name' => 'web',
+            ], [
+                'name ' => 'konfigurasi toko',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'pengaturan cetak struk',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'integrasi sistem',
+                'guard_name' => 'web',
+            ], [
+                'name' => 'edit toko',
+                'guard_name' => 'web',
+            ]
         ];
 
         foreach ($permission as $key => $value) {
